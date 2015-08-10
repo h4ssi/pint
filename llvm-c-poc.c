4c5afba01a2afe55b112f180a7a3f348aaaded23
Florian Hassanen <florian.hassanen@gmail.com>
2015-08-10 22:35:05 +0200

    generating a simple function with clang c interface

--- llvm-c-poc.c
+++ llvm-c-poc.c
@@ -1,7 +1,23 @@
 #include <llvm-c/Core.h>
+#include <llvm-c/Analysis.h>
 
 int main() {
+  LLVMContextRef c = LLVMGetGlobalContext();
+  LLVMModuleRef m = LLVMModuleCreateWithNameInContext("test", c);
+  LLVMTypeRef i = LLVMInt32TypeInContext(c);
+  LLVMTypeRef ps[] = {i, i};
+  LLVMTypeRef ft = LLVMFunctionType(i, ps, 2, 0);
+  LLVMValueRef f = LLVMAddFunction(m, "plus", ft);
+  LLVMBasicBlockRef bb = LLVMAppendBasicBlockInContext(c, f, "entry");
   LLVMBuilderRef b = LLVMCreateBuilder();
+  LLVMPositionBuilderAtEnd(b, bb);
+  LLVMValueRef r =
+      LLVMBuildNSWAdd(b, LLVMGetParam(f, 0), LLVMGetParam(f, 1), "");
+  LLVMBuildRet(b, r);
+  LLVMVerifyFunction(f, LLVMPrintMessageAction);
+  LLVMVerifyModule(m, LLVMPrintMessageAction, 0);
   LLVMDisposeBuilder(b);
+
+  LLVMDumpModule(m);
   return 0;
 }
